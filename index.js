20231546a656d5c3a96992afaecaaeaf0824a0c2
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-18 00:13:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
 const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
-  driver.key = value;
+  return Object.assign({}, object, {[key]:value});
 }
