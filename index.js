34a2976916131ac1c975a2b11f3291d9a57c65c5
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-18 00:52:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ function destructivelyUpdateDriverWithKeyAndValue(object,key,value) {
   object[key] = value;
   return object;
 }
-function deleteFromDriverByKey(driver, key) {
-  const newObj = Object.assign({}, driver);
-  delete newObj[key];
-  return newObj
-  })
+
 function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver[key];
   return driver;
